1a523864f0bf093354b7fd1ae3eeb749aecf817c
tamimmmmm54-prog <tamimmmmm54@gmail.com>
2026-09-08 20:21:18 +0600

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -9,11 +9,11 @@ st.sidebar.header('Dataset & Preprocessing')
 
 from sklearn.datasets import load_iris,load_wine,load_breast_cancer
 def load_sample(name):
-  if name == 'Sample dataset (Iris)':
+  if name == 'Sample dataset(Iris)':
     df = load_iris(as_frame= True)
-  elif name == 'Sample dataset (Wine)':
+  elif name == 'Sample dataset(Wine)':
     df = load_wine(as_frame=True)
-  elif name == 'Sample dataset (Breast Cancer)':
+  elif name == 'Sample dataset(Breast Cancer)':
     df = load_breast_cancer(as_frame = True)
   else:
       return None
